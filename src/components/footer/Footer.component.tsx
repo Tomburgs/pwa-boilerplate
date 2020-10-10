f65b8848100eec449a9fefedc43ed52453a839e5
Toms Burgmanis <toms.burgmanis@gmail.com>
2020-10-10 18:09:04 +0300

    Add footer with GitHub link

---
+++ src/components/footer/Footer.component.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const {
+    footer
+} = styles;
+
+export default function Footer(): JSX.Element {
+    return (
+        <footer className={ footer }>
+            <p>© PWA Boilerplate</p>
+            <p>
+                View source code on <a
+                  href="https://github.com/tomburgs/pwa-boilerplate"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                    GitHub
+                </a>
+            </p>
+        </footer>
+    );
+}
