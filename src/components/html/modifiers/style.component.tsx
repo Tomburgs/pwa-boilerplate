8bb828ddbf0cdaf8350413077223ae23108197d5
Toms Burgmanis <toms.burgmanis@gmail.com>
2021-01-16 12:12:47 +0200

    Fix html/modifiers/style type for DomNode

--- src/components/html/modifiers/style.component.tsx
+++ src/components/html/modifiers/style.component.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { DomElement } from 'htmlparser2';
+import { Element } from 'domhandler/lib/node';
 import Head from 'next/head';
 
-export default function Style(domNode: DomElement): JSX.Element | null {
+export default function Style(domNode: Element): JSX.Element | null {
     const { children } = domNode;
 
     return (
