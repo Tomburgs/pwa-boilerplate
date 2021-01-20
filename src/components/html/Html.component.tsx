39b80e23f9be333882b26e01255e01d8620f58e6
Toms Burgmanis <toms.burgmanis@gmail.com>
2021-01-20 20:42:44 +0200

    Fix html parser instanceof (#183)
    * Fix html-react-parser instanceof not working
    
    * remove @babel/plugin-transform-instanceof from dependencies
    
    * Fix html-react-parser instanceof in prod build
    
    * Fix Element import source

--- src/components/html/Html.component.tsx
+++ src/components/html/Html.component.tsx
@@ -20,10 +20,10 @@ export const components: { [key: string]: any } = {
 export const options: HTMLReactParserOptions = {
     /*
      * Node set as `any` due to babel es5 transpilation which breaks `instanceof`.
-     * Instead of checking `instanceof` we check constructor name.
+     * Instead of checking `instanceof` we check `attribs`.
      */
     replace: (node: any) => {
-        if (node.constructor.name === 'Element' && node.attribs) {
+        if (node.attribs) {
             const { name = '', attribs, children } = node as Element;
             const component = components[name];
 
