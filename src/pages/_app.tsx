53d132d92d0c19ff1a15cab6a22a26f7ad497fcf
Hyeonss <skyoun97@gmail.com>
2021-01-27 19:41:17 +0900

    Disable pinch to zoom-in & zoom-out
    I think it is a better user experience to disable pinch to zoom-in & zoom-out because it is rarely used in app environment.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1, user-scalable=0, viewport-fit=cover" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content={ statusBarStyle } />
                 <link rel="apple-touch-startup-image" sizes="512x512" href="/logo512.png" />
